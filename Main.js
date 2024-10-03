96dcb4c8fd0d86efa09fed5f488ac31c0d6b1aa7
BSK_BE4T5 <panfilodemon1@gmail.com>
2024-10-02 22:07:05 -0600

    a

--- Main.js
+++ Main.js
@@ -1,7 +1,7 @@
 "use sctrict";
 
-var __assing = this && this._assing || function() {
-  __assing = Object.assign || function (t){
+var __assign = this && this._assing || function() {
+  __assign = Object.assign || function (t){
     for (var s, i = 1, n = arguments.length; i<n; i++){
       s = arguments[i];
       for (var p in s ) if (Object.prototype.hasOwnProperty.call(s, p))
@@ -9,7 +9,7 @@ var __assing = this && this._assing || function() {
     }
     return t;
   };
-  return __assing.apply(this, arguments);
+  return __assign.apply(this, arguments);
 };
 
 var shuffel = function(_a){
@@ -39,6 +39,59 @@ var randomInArrayInRange = function (min, max, n){
 };
 
 var Droppable = /** @class */function(){
+  function Droppable (el) {
+    this.droppableEl = el;
+  }
+  Droppable.prototype.isDroppable = function (droppableEl) {
+    var draggableOffset = getOffset(draggableEl);
+    var droppableOffset = getOffset(this.droppableEl);
 
-  
-}
+    var _a = [draggableEl.offsetwidth, draggableEl.offsetHeight], draggablewidth = _a[0], draggableHeight =  _a[1];
+    var _b = [this.droppableEl.offsetwidth, this.droppableEl.offsetHeight], droppableWidth = _b[0], droppableHeight =  _b[1];
+
+    return !(droppableOffset.left > draggableOffset.left + draggableWidth - draggablewidth - draggableWidth / 2 || 
+
+      droppableOffset.left + droppableWidt < draggableOffset.left + draggableWidth / 2 ||
+
+      droppableOffset.top > draggableOffset.top + draggableHeight - draggableHeight /2 ||
+
+      droppableOffset.top + droppableHeight < draggableOffset.top + draggableHeight /2);
+
+  };
+  return Droppable;
+
+
+
+}();
+
+var Draggable = /**@class */function (){
+  function Draggable(el){
+    this.draggableEl = el;
+    this.draggie = new draggabilly(el);
+    this.originPos = __assign
+    
+
+  }
+  return Draggable;
+
+
+
+}();
+
+var randomBlockWidths = randomInArrayInRange(30, 60, 10);
+
+var randomBlockBorderRadiuses = randomInArrayInRange(1,30,10);
+
+var draggableBlocks = document.querySelectorAll('.block.draggable');
+
+var targetBlocks = document.querySelectorAll('block.target');
+
+var startBtn = document.querySelector('#start');
+
+var scoreNumber = document.querySelector(".score-number");
+
+var timeLeftNumber = document.querySelector("time-left-number");
+
+var finalScoreDialog = document.querySelector("#final-score-dialog");
+
+var fi
